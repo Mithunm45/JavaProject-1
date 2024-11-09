c774471c9d69d8257eb779e53b5908544cd5976f
Mithunm45 <mithunrajm007@gmail.com>
2024-11-09 20:25:09 +0530

    implemented interface methods

--- src/Student/Student.java
+++ src/Student/Student.java
@@ -1,6 +1,6 @@
 package Student;
 
-public class Student {
+public class Student implements Activity{
 
     private String name;
     private String course;
@@ -16,5 +16,15 @@ public class Student {
         System.out.println("Name ->"+name);
         System.out.println("Course ->"+course);
         System.out.println("age ->"+age);
+    }
+
+    @Override
+    public void exam() {
+        System.out.println("Exam of "+course+" is tough");
+    }
+
+    @Override
+    public void marks() {
+        System.out.println("Minimum percentage needed to pass is 50.5");
     }
 }
